974f2352e913deb57651737aa5e343620a120c48
poppy3 <naamaah@post.bgu.ac.il>
2024-12-04 09:41:00 +0200

    data

--- Audio.py
+++ Audio.py
@@ -30,12 +30,14 @@ def say(str_to_say):
     This function make the robot say whatever there is in the file - play the audio (paralelly)
     :return: audio
     '''
-    mixer.init()
-    sound = mixer.Sound(s.audio_path+str_to_say+'.wav')
-    audio_length = sound.get_length()
-    sound.play()
-    time.sleep(audio_length-1)
-
+    try:
+        mixer.init()
+        sound = mixer.Sound(s.audio_path+str_to_say+'.wav')
+        audio_length = sound.get_length()
+        sound.play()
+        time.sleep(audio_length-1)
+    except Exception as e:
+        print(f"error sound not found: {e}")
 
 if __name__ == '__main__':
     language = 'Hebrew'
